bad6bc9dc06f8be8a4ec6bfb9262eeebd40db14e
Alex Kerney <abk@mac.com>
2017-10-01 17:24:42 -0400

    Adding more tests and splitting up requirements

--- tests/test_email_to.py
+++ tests/test_email_to.py
@@ -6,20 +6,53 @@
 import pytest
 
 
-from email_to import email_to
+import email_to
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def message():
+    """ Simple message """
+    msg = email_to.Message('# A Test Message', style='h1 { color: blue }')
+    msg.add('This message is only a test of the alert system')
+    return msg
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_build_message(message):
+    """ Test that a message can be created and added to directly """
+    msg = message
+
+    msg_string = str(msg)
+    assert '# A Test Message' in msg_string
+    assert 'This message is only a test of the alert system' in msg_string
+
+    msg_html = msg.html
+    assert '<html>' in msg_html
+    assert '<head></head>' in msg_html
+    assert '<body>' in msg_html
+    assert 'style="color:blue">' in msg_html
+    assert 'A Test Message' in msg_html
+    assert '<p>This message is only a test of the alert system' in msg_html
+
+
+def test_empty_message():
+    """ Test that a message passed an empty body gets created """
+    msg = email_to.Message()
+    msg_string = str(msg)
+    assert msg_string == ''
+
+    msg_html = msg.html
+    assert msg.html == ''
+
+
+def test_message_mime(message):
+    """ Test that a message can form a valid MIMEMultiPart object """
+    mime = message.mime()
+    mime_string = str(mime)
+    assert 'Content-Type' in mime_string
+    assert 'multipart/alternative' in mime_string
+    #assert 'From' in mime_string
+    assert 'MIME-Version:' in mime_string
+    assert 'Content-Type: text/plain; charset="us-ascii"' in mime_string
+    assert '# A Test Message' in mime_string
+    assert 'Content-Type: text/html; charset="us-ascii"' in mime_string
+    assert '<h1 style="color:blue">A Test Message</h1>' in mime_string
